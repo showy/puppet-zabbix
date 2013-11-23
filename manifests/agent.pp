9a4f9756ddcfca9dabe6b3456d29d6f12cf289c1
ShOwY <showy@t430.localdomain>
2013-11-23 18:30:31 -0430

    Temp Commit

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -1,12 +1,28 @@
 class puppet-zabbix::agent {
-	case $lsbdistcodename {
-		/(?i:debian)/: {
-			$zabbix_pkgs = [ 'zabbix-agent']
-			$zabbix_server_cfg = '/etc/zabbix/zabbix_agent.cfg'
-			$have_distro? = True
+	# Include priority classes
+	if (!defined(puppet-zabbix::settings)){
+		include puppet-zabbix::settings
+	}
+	if (!defined(puppet-zabbix::base)) {
+		include puppet-zabbix::base
+	}
+
+	# Ensure ordering
+	Class["puppet-zabbix::settings"] ->
+	Class["puppet-zabbix::base"] ->
+	Class['puppet-zabbix::agent']
+
+	# this->specific
+	if ($support_os_agent){
+		case $lsbdistcodename {
+			/(?i:debian)/: {
+				$zabbix_pkgs = [ 'zabbix-agent']
+				$zabbix_server_cfg = '/etc/zabbix/zabbix_agent.cfg'
+				$have_distro = true
+			}
 		}
-	}
-	if ($have_distro?) {
-		notify {"Zabbix::Agent dot pp":}
-	}
+		if ($have_distro) {
+			notify {"Zabbix::Agent dot pp":}
+		}
+	}	
 }
--- manifests/server.pp
+++ manifests/server.pp
@@ -1,12 +1,28 @@
 class puppet-zabbix::server {
-	# This class provider install a zabbix server for debian wheezy
-	case $lsbdistcodename {
-		/(?i:debian): {
-			$zabbix_pkgs = [ 'zabbix-server']
-			$zabbix_server_cfg = '/etc/zabbix/zabbix.cfg'
-			$have_distro? = True
-		}
+	# Include priority classes
+	if (!defined(puppet-zabbix::settings)) {
+		include puppet-zabbix::settings
 	}
-	if ($have_distro?) {
+	if (!defined(puppet-zabbix::base)) {
+		include puppet-zabbix::base
+	}
+
+	# Ensure ordering
+	Class["puppet-zabbix::settings"] ->
+	Class["puppet-zabbix::base"] ->
+	Class["puppet-zabbix::server"]
+
+	# this->specific
+	if ($support_os_server) {
+		# This class provider install a zabbix server for debian wheezy
+		case $lsbdistcodename {
+			/(?i:debian): {
+				$zabbix_pkgs = [ 'zabbix-server']
+				$zabbix_server_cfg = '/etc/zabbix/zabbix.cfg'
+				$have_distro = true
+			}
+		}
+		if ($have_distro) {
+		}
 	}
 }
